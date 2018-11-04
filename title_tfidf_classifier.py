e31ed25cf435c3fc97440f8b27ab146713acc188
Shenghao1993 <wangshenghao1993@gmail.com>
2018-11-04 23:01:35 +0800

    Compute sentiment metrics for articles

--- title_tfidf_classifier.py
+++ title_tfidf_classifier.py
@@ -2,6 +2,8 @@ import os
 import logging
 import numpy as np
 import pandas as pd
+import xgboost as xgb
+import time
 from logging.config import fileConfig
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import KFold
@@ -34,8 +36,11 @@ def main():
     avg_p = 0
     avg_r = 0
     avg_f1 = 0
+    start_time = time.time()
     for train, test in kf.split(x):
-        model = MultinomialNB().fit(x[train], y[train])
+        #model = MultinomialNB().fit(x[train], y[train])
+        model = xgb.XGBClassifier(max_depth=5, learning_rate=0.1,
+                                  n_estimators=140).fit(x[train], y[train])
         predicts = model.predict(x[test])
         logger.info(classification_report(y[test], predicts))
         avg_a += accuracy_score(y[test], predicts)
@@ -47,6 +52,8 @@ def main():
     logger.info('Average Precision is %f.' %(avg_p/10.0))
     logger.info('Average Recall is %f.' %(avg_r/10.0))
     logger.info('Average F1 score is %f.' %(avg_f1/10.0))
+    logger.info("Elapsed time: %s seconds...",
+                round(time.time() - start_time, 4))
 
 
 if __name__ == "__main__":
